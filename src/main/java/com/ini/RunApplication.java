8c14f75bf78f6864d3e1c7f7030e19d4aab10240
chen_jian <chenjian190000774@126.com>
2018-10-18 09:20:22 +0800

    20181018

--- src/main/java/com/ini/RunApplication.java
+++ src/main/java/com/ini/RunApplication.java
@@ -1,7 +1,7 @@
 package com.ini;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class RunApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunApplication.class, args);
+		//SpringApplication.run(RunApplication.class, args);
+		new SpringApplicationBuilder(RunApplication.class).web(true).run(args);
 	}
 }
